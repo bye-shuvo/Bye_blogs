2d925ecb07edf87be3966dfa6761a203d416b1a4
bye <abuhasnatshuvo605@gmail.com>
2026-02-22 00:55:28 +0600

    Added Logic for home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import Input from "./Input";
+import PostDisplay from "./PostDisplay";
 
-const Home = () => {
+function Home() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [blogPosts, setBlogPosts] = useState([]);
+  function handlePost() {
+    setTitle('');
+    setDescription('');
+    if (title && description) {
+      setBlogPosts(prev => [...prev, { title, description }]);
+    }
+  }
   return (
-    <div>
-      
+    <div className="text-center ma-20">
+      <div className="mb-20">
+        <Input title={title} setTitle={setTitle} description={description} setDescription={setDescription} />
+        <button data-testid="create-button" className="mt-10" onClick={handlePost}>
+          Create Post
+        </button>
+      </div>
+      <div className="posts-section">
+        <PostDisplay blogPosts={blogPosts} setBlogPosts={setBlogPosts} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
